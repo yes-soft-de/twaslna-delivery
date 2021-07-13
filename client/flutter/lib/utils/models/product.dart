6f0b9a0409e50249d67252bbd3137b42d6d97f19
Yazan99sh <syria.yazan19@gmail.com>
2021-07-14 01:05:07 +0300

    chenge price data type to double

--- client/flutter/lib/utils/models/product.dart
+++ client/flutter/lib/utils/models/product.dart
@@ -1,7 +1,7 @@
 class ProductModel {
   String title;
   String image;
-  int price;
+  double price;
   int id;
   int quantity;
   ProductModel({required this.title,required this.image,required this.price,required this.id,this.quantity = 0});
