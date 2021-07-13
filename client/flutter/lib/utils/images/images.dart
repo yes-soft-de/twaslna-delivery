29162ad541da43db51a9c54687a80a01559d9593
Yazan99sh <syria.yazan19@gmail.com>
2021-07-14 01:04:37 +0300

    new asset has been added

--- client/flutter/lib/utils/images/images.dart
+++ client/flutter/lib/utils/images/images.dart
@@ -3,7 +3,7 @@ class ImageAsset {
   static const LOGO = ROOT + 'logo.png';
   static const SEND_ON_ME = ROOT + 'send_on_me.jpg';
   static const STORE_CATEGORY_SUPER_MARKET = ROOT + 'super_market.jpg';
-
-
+  static const AUTH_SVG = ROOT + 'login.svg';
+  static const DELIVERY_MOTOR = ROOT + 'delivery_splash.png';
 
 }
