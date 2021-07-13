e81f9e8d060da9db27f401e7ee0d309a9bb31d71
Yazan99sh <syria.yazan19@gmail.com>
2021-07-14 00:41:45 +0300

    hide empty tiles

--- client/flutter/lib/module_home/ui/state/home_loaded_state.dart
+++ client/flutter/lib/module_home/ui/state/home_loaded_state.dart
@@ -5,6 +5,7 @@ import 'package:twaslna_delivery/module_home/ui/state/home_state.dart';
 import 'package:twaslna_delivery/module_home/ui/widget/home_app_bar.dart';
 import 'package:twaslna_delivery/module_home/ui/widget/product_card.dart';
 import 'package:twaslna_delivery/module_home/ui/widget/show_all.dart';
+import 'package:twaslna_delivery/module_our_services/services_routes.dart';
 import 'package:twaslna_delivery/module_stores/store_routes.dart';
 import 'package:twaslna_delivery/utils/customIcon/custom_icons.dart';
 import 'package:twaslna_delivery/utils/images/images.dart';
@@ -18,7 +19,10 @@ class HomeLoadedState extends HomeState {
   List<ProductModel> topProducts;
   List<StoreCategoryModel> categories;
   List<StoreModel> bestStores;
-  HomeLoadedState(this.screenState,this.topProducts,this.categories,this.bestStores) : super(screenState);
+
+  HomeLoadedState(
+      this.screenState, this.topProducts, this.categories, this.bestStores)
+      : super(screenState);
 
   @override
   Widget getUI(BuildContext context) {
@@ -26,71 +30,135 @@ class HomeLoadedState extends HomeState {
       physics: BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
       children: [
         CustomHomeAppBar(),
-        ListTile(leading: Icon(CustomIcon.our_service,color: Theme.of(context).primaryColor,size: 18,),
-          title: Text(S.of(context).ourService,style:StyleText.categoryStyle,),
+        ListTile(
+          leading: Icon(
+            CustomIcon.our_service,
+            color: Theme.of(context).primaryColor,
+            size: 18,
+          ),
+          title: Text(
+            S.of(context).ourService,
+            style: StyleText.categoryStyle,
+          ),
         ),
         SizedBox(
           height: 125,
           child: ListView(
             scrollDirection: Axis.horizontal,
-            physics: BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
+            physics:
+                BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
             children: [
-              HomeCard(title:S.of(context).deliverForMe, image:ImageAsset.SEND_ON_ME),
-              HomeCard(title: S.of(context).externalOrder, image:ImageAsset.LOGO),
+              HomeCard(
+                title: S.of(context).deliverForMe,
+                image: ImageAsset.SEND_ON_ME,
+                onTap: () {
+                  Navigator.of(context).pushNamed(ServicesRoutes.SEND_IT);
+                },
+              ),
             ],
           ),
         ),
-        ListTile(leading: Icon(CustomIcon.top_product,color: Theme.of(context).primaryColor,size: 18,),
-          title: Text(S.of(context).mostSoldProduct,style: StyleText.categoryStyle,),
-          trailing:showAll(context),
-        ),
-        SizedBox(
-          height: 125,
-          child: ListView(
-            physics: BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
-            scrollDirection: Axis.horizontal,
-            children:_getTopProducts(topProducts),
+        topProducts.isNotEmpty
+            ? ListTile(
+                leading: Icon(
+                  CustomIcon.top_product,
+                  color: Theme.of(context).primaryColor,
+                  size: 18,
+                ),
+                title: Text(
+                  S.of(context).mostSoldProduct,
+                  style: StyleText.categoryStyle,
+                ),
+                trailing: showAll(context),
+              )
+            : Container(),
+        topProducts.isNotEmpty
+            ? SizedBox(
+                height: 125,
+                child: ListView(
+                  physics: BouncingScrollPhysics(
+                      parent: AlwaysScrollableScrollPhysics()),
+                  scrollDirection: Axis.horizontal,
+                  children: _getTopProducts(topProducts),
+                ),
+              )
+            : Container(),
+        bestStores.isNotEmpty
+            ? ListTile(
+                leading: Icon(
+                  CustomIcon.top_store,
+                  color: Theme.of(context).primaryColor,
+                  size: 18,
+                ),
+                title: Text(
+                  S.of(context).bestStore,
+                  style: StyleText.categoryStyle,
+                ),
+                trailing: showAll(context),
+              )
+            : Container(),
+        bestStores.isNotEmpty
+            ? SizedBox(
+                height: 125,
+                child: ListView(
+                  physics: BouncingScrollPhysics(
+                      parent: AlwaysScrollableScrollPhysics()),
+                  scrollDirection: Axis.horizontal,
+                  children: getBestStores(bestStores),
+                ),
+              )
+            : Container(),
+        ListTile(
+          leading: Icon(
+            CustomIcon.near_me,
+            color: Theme.of(context).primaryColor,
+            size: 18,
           ),
-        ),
-        ListTile(leading: Icon(CustomIcon.top_store,color: Theme.of(context).primaryColor,size: 18,),
-          title: Text(S.of(context).bestStore,style: StyleText.categoryStyle,),
-          trailing:showAll(context),
-        ),
-        SizedBox(
-          height: 125,
-          child: ListView(
-            physics: BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
-            scrollDirection: Axis.horizontal,
-            children:getBestStores(bestStores),
+          title: Text(
+            S.of(context).nearbyStore,
+            style: StyleText.categoryStyle,
           ),
-        ),
-        ListTile(leading: Icon(CustomIcon.near_me,color: Theme.of(context).primaryColor,size: 18,),
-          title: Text(S.of(context).nearbyStore,style: StyleText.categoryStyle,),
-          trailing:showAll(context),
+          trailing: showAll(context),
         ),
         SizedBox(
           height: 125,
           child: ListView.builder(
-            physics: BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
+            physics:
+                BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
             scrollDirection: Axis.horizontal,
             itemCount: 10,
             itemBuilder: (_, index) {
-              return HomeCard(title:'متجر', image: 'https://media-cdn.tripadvisor.com/media/photo-s/17/75/3f/d1/restaurant-in-valkenswaard.jpg');
+              return HomeCard(
+                  title: 'متجر',
+                  image:
+                      'https://media-cdn.tripadvisor.com/media/photo-s/17/75/3f/d1/restaurant-in-valkenswaard.jpg');
             },
           ),
         ),
-        ListTile(leading: Icon(Icons.sort,color: Theme.of(context).primaryColor,),
-          title: Text(S.of(context).categories,style:StyleText.categoryStyle,),
-        ),
-        GridView(
-          physics: ScrollPhysics(),
-          shrinkWrap: true,
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              mainAxisSpacing: 8.0,
-              childAspectRatio: (MediaQuery.of(context).size.width / 2) / 135),
-          children: getCategories(categories),
-        ),
+        categories.isNotEmpty
+            ? ListTile(
+                leading: Icon(
+                  Icons.sort,
+                  color: Theme.of(context).primaryColor,
+                ),
+                title: Text(
+                  S.of(context).categories,
+                  style: StyleText.categoryStyle,
+                ),
+              )
+            : Container(),
+        categories.isNotEmpty
+            ? GridView(
+                physics: ScrollPhysics(),
+                shrinkWrap: true,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 8.0,
+                    childAspectRatio:
+                        (MediaQuery.of(context).size.width / 2) / 135),
+                children: getCategories(categories),
+              )
+            : Container(),
         SizedBox(
           height: 75,
         ),
@@ -102,7 +170,10 @@ class HomeLoadedState extends HomeState {
     if (topProducts.isEmpty) return [];
     List<HomeCard> top = [];
     topProducts.forEach((element) {
-      top.add(HomeCard(title:element.title,image: element.image,));
+      top.add(HomeCard(
+        title: element.title,
+        image: element.image,
+      ));
     });
     return top;
   }
@@ -111,21 +182,28 @@ class HomeLoadedState extends HomeState {
     if (categories.isEmpty) return [];
     List<HomeCard> cats = [];
     categories.forEach((element) {
-      cats.add(HomeCard(title:element.storeCategoryName,image: element.image,
-      onTap: () {
-        Navigator.of(screenState.context).pushNamed(StoreRoutes.STORE_LIST_SCREEN,arguments:element);
-      },
+      cats.add(HomeCard(
+        title: element.storeCategoryName,
+        image: element.image,
+        onTap: () {
+          Navigator.of(screenState.context)
+              .pushNamed(StoreRoutes.STORE_LIST_SCREEN, arguments: element);
+        },
       ));
     });
     return cats;
   }
+
   List<Widget> getBestStores(List<StoreModel> stores) {
     if (stores.isEmpty) return [];
     List<HomeCard> bestStoresCards = [];
     stores.forEach((element) {
-      bestStoresCards.add(HomeCard(title:element.storeOwnerName,image: element.image,
+      bestStoresCards.add(HomeCard(
+        title: element.storeOwnerName,
+        image: element.image,
         onTap: () {
-          Navigator.of(screenState.context).pushNamed(StoreRoutes.STORE_PRODUCTS,arguments:element);
+          Navigator.of(screenState.context)
+              .pushNamed(StoreRoutes.STORE_PRODUCTS, arguments: element);
         },
       ));
     });
