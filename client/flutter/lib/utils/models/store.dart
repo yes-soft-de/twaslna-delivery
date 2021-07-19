ba4125e93065aa28035abe40ecdda9fc23a19c7f
Yazan99sh <syria.yazan19@gmail.com>
2021-07-19 09:54:12 +0300

    new models

--- client/flutter/lib/utils/models/store.dart
+++ client/flutter/lib/utils/models/store.dart
@@ -4,7 +4,7 @@ class StoreModel {
   String image;
   dynamic phone;
   dynamic location;
-
-  StoreModel({
+  double deliveryCost;
+  StoreModel({required this.deliveryCost,
       required this.id, required this.storeOwnerName,required this.image, this.phone, this.location});
 }
