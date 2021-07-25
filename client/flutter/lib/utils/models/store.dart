9b0fd565ed4592d85fd8bd828ce11a234ba78654
Yazan99sh <syria.yazan19@gmail.com>
2021-07-25 23:56:02 +0300

    add nes fields

--- client/flutter/lib/utils/models/store.dart
+++ client/flutter/lib/utils/models/store.dart
@@ -5,6 +5,16 @@ class StoreModel {
   dynamic phone;
   dynamic location;
   double deliveryCost;
-  StoreModel({required this.deliveryCost,
-      required this.id, required this.storeOwnerName,required this.image, this.phone, this.location});
+  bool hasProducts;
+  bool privateOrders;
+
+  StoreModel(
+      {required this.deliveryCost,
+      required this.id,
+      required this.storeOwnerName,
+      required this.image,
+      this.phone,
+      this.location,
+      required this.hasProducts,
+      required this.privateOrders});
 }
