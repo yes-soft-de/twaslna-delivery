3fd1a414236bb1dcf3cfc18a5c58037c190ecc33
Yazan99sh <syria.yazan19@gmail.com>
2021-08-01 00:18:32 +0300

    costomize model

--- client/flutter/lib/utils/models/store.dart
+++ client/flutter/lib/utils/models/store.dart
@@ -1,12 +1,19 @@
+import 'package:twaslna_delivery/generated/l10n.dart';
+import 'package:twaslna_delivery/module_home/response/best_store.dart';
+import 'package:twaslna_delivery/module_stores/response/store_category_list.dart';
+
 class StoreModel {
-  int id;
-  String storeOwnerName;
-  String image;
+  late int id;
+  late String storeOwnerName;
+  late String image;
   dynamic phone;
   dynamic location;
-  double deliveryCost;
-  bool hasProducts;
-  bool privateOrders;
+  late double deliveryCost;
+  late bool hasProducts;
+  late bool privateOrders;
+  String? error;
+  bool empty = false;
+  List<StoreModel> models = [];
 
   StoreModel(
       {required this.deliveryCost,
@@ -17,4 +24,32 @@ class StoreModel {
       this.location,
       required this.hasProducts,
       required this.privateOrders});
+
+  StoreModel.Empty() {
+    this.empty = true;
+  }
+
+  StoreModel.Error(this.error);
+
+  StoreModel.Data(StoreCategoryList bestStores) {
+    var data = bestStores.data;
+    data!.forEach((element) {
+      models.add(StoreModel(
+          id: element.id ?? -1,
+          storeOwnerName: element.storeOwnerName ?? S.current.store,
+          deliveryCost: element.deliveryCost ?? 0,
+          hasProducts: element.hasProducts ?? false,
+          privateOrders: element.privateOrders ?? false,
+          image:
+              'https://www.gannett-cdn.com/media/2020/03/23/USATODAY/usatsports/247WallSt.com-247WS-657876-imageforentry9-vp7.jpg?width=660&height=371&fit=crop&format=pjpg&auto=webp'));
+    });
+  }
+
+  bool get hasError => error != null;
+
+  bool get isEmpty => empty;
+
+  List<StoreModel> get data {
+    return models;
+  }
 }
