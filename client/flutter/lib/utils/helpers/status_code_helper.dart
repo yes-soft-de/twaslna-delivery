490696b06799c1798b6a5e4806e2fbc20153c5b0
Yazan99sh <syria.yazan19@gmail.com>
2021-08-01 00:19:53 +0300

    internal server error

--- client/flutter/lib/utils/helpers/status_code_helper.dart
+++ client/flutter/lib/utils/helpers/status_code_helper.dart
@@ -1,20 +1,25 @@
 import 'package:twaslna_delivery/generated/l10n.dart';
 
 class StatusCodeHelper {
-  static String getStatusCodeMessages(int statusCode) {
-      switch (statusCode) {
-      case 200 :
+  static String getStatusCodeMessages(var statusCode) {
+    if (statusCode is int) {
+      statusCode = statusCode.toString();
+    }
+    switch (statusCode) {
+      case '200':
         return S.current.statusCodeOk;
-      case 201:
+      case '201':
         return S.current.statusCodeCreated;
-      case 400:
+      case '400':
         return S.current.statusCodeBadRequest;
-      case 401:
+      case '401':
         return S.current.statusCodeUnauthorized;
-      case 404:
+      case '404':
         return S.current.StatusCodeNotFound;
+      case '500':
+        return S.current.internalServerError;
       default:
-        return S.current.networkError;
+        return S.current.errorHappened;
     }
   }
 }
