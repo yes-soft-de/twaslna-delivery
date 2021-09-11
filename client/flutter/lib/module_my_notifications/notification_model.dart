84bc1e1e16564d447c4c55a807407a6ec6a9c2f2
Yazan99sh <syria.yazan19@gmail.com>
2021-09-11 22:19:13 +0300

    local notification added

--- client/flutter/lib/module_my_notifications/notification_model.dart
+++ client/flutter/lib/module_my_notifications/notification_model.dart
@@ -1,19 +1,17 @@
 import 'package:intl/intl.dart';
-import 'package:twaslna_delivery/consts/order_status.dart';
 import 'package:twaslna_delivery/generated/l10n.dart';
 import 'package:twaslna_delivery/module_my_notifications/response/my_notification_response.dart';
-import 'package:twaslna_delivery/module_orders/response/my_orders_response.dart';
-import 'package:twaslna_delivery/utils/helpers/order_status_helper.dart';
 
 class NotificationModel {
   String orderNumber = '-1';
   String body = S.current.orderDetails;
   String title = S.current.orderNumber;
+  String date = '';
   String? error;
   bool empty = false;
   List<NotificationModel> models = [];
 
-  NotificationModel({required this.orderNumber,required this.title,required this.body});
+  NotificationModel({required this.orderNumber,required this.title,required this.body,required this.date});
 
   NotificationModel.Empty() {
     this.empty = true;
@@ -21,16 +19,17 @@ class NotificationModel {
   NotificationModel.Error(this.error);
 
   NotificationModel.Data(MyNotificationResponse orders) {
-    var data = orders.data?.order;
+    var data = orders.data;
     data?.forEach((element) {
-      // String date = DateFormat('dd-MM-yyyy').format(DateTime.fromMillisecondsSinceEpoch(
-      //     (element.deliveryDate?.timestamp ??
-      //         DateTime.now().millisecondsSinceEpoch) *
-      //         1000));
+      String notificationDate = DateFormat.jm().format(DateTime.fromMillisecondsSinceEpoch(
+          (element.createdAt?.timestamp ??
+              DateTime.now().millisecondsSinceEpoch) *
+              1000));
       models.add(NotificationModel(
-        title: title + ' ' +element.orderNumber.toString(),
+        title: element.title ?? '',
         orderNumber: element.orderNumber.toString(),
-        body: body
+        body: element.message ??'',
+        date:notificationDate
       ));
     });
   }
--- client/flutter/lib/module_my_notifications/response/my_notification_response.dart
+++ client/flutter/lib/module_my_notifications/response/my_notification_response.dart
@@ -1,9 +1,7 @@
-import 'package:twaslna_delivery/utils/logger/logger.dart';
-
 class MyNotificationResponse {
   String? statusCode;
   String? msg;
-  Data? data;
+  List<Data>? data;
 
   MyNotificationResponse({
       this.statusCode, 
@@ -11,14 +9,13 @@ class MyNotificationResponse {
       this.data});
 
   MyNotificationResponse.fromJson(dynamic json) {
-    try {
-      statusCode = json['status_code'];
-      msg = json['msg'];
-      data = json['Data'] != null ? Data.fromJson(json['Data']) : null;
-    }
-    catch (e){
-      Logger().error('Notification Response',e.toString(), StackTrace.current);
-      statusCode = '-1';
+    statusCode = json['status_code'];
+    msg = json['msg'];
+    if (json['Data'] != null) {
+      data = [];
+      json['Data'].forEach((v) {
+        data?.add(Data.fromJson(v));
+      });
     }
   }
 
@@ -27,7 +24,7 @@ class MyNotificationResponse {
     map['status_code'] = statusCode;
     map['msg'] = msg;
     if (data != null) {
-      map['Data'] = data?.toJson();
+      map['Data'] = data?.map((v) => v.toJson()).toList();
     }
     return map;
   }
@@ -35,43 +32,163 @@ class MyNotificationResponse {
 }
 
 class Data {
-  List<Order>? order;
+  int? id;
+  String? title;
+  String? message;
+  int? orderNumber;
+  CreatedAt? createdAt;
 
   Data({
-      this.order});
+      this.id, 
+      this.title, 
+      this.message, 
+      this.orderNumber, 
+      this.createdAt});
 
   Data.fromJson(dynamic json) {
-    if (json['order'] != null) {
-      order = [];
-      json['order'].forEach((v) {
-        order?.add(Order.fromJson(v));
-      });
-    }
+    id = json['id'];
+    title = json['title'];
+    message = json['message'];
+    orderNumber = json['orderNumber'];
+    createdAt = json['createdAt'] != null ? CreatedAt.fromJson(json['createdAt']) : null;
   }
 
   Map<String, dynamic> toJson() {
     var map = <String, dynamic>{};
-    if (order != null) {
-      map['order'] = order?.map((v) => v.toJson()).toList();
+    map['id'] = id;
+    map['title'] = title;
+    map['message'] = message;
+    map['orderNumber'] = orderNumber;
+    if (createdAt != null) {
+      map['createdAt'] = createdAt?.toJson();
     }
     return map;
   }
 
 }
 
-class Order {
-  String? orderNumber;
+class CreatedAt {
+  Timezone? timezone;
+  int? offset;
+  int? timestamp;
 
-  Order({
-      this.orderNumber});
+  CreatedAt({
+      this.timezone, 
+      this.offset, 
+      this.timestamp});
 
-  Order.fromJson(dynamic json) {
-    orderNumber = json['orderNumber'];
+  CreatedAt.fromJson(dynamic json) {
+    timezone = json['timezone'] != null ? Timezone.fromJson(json['timezone']) : null;
+    offset = json['offset'];
+    timestamp = json['timestamp'];
   }
 
   Map<String, dynamic> toJson() {
     var map = <String, dynamic>{};
-    map['orderNumber'] = orderNumber;
+    if (timezone != null) {
+      map['timezone'] = timezone?.toJson();
+    }
+    map['offset'] = offset;
+    map['timestamp'] = timestamp;
+    return map;
+  }
+
+}
+
+class Timezone {
+  String? name;
+  List<Transitions>? transitions;
+  Location? location;
+
+  Timezone({
+      this.name, 
+      this.transitions, 
+      this.location});
+
+  Timezone.fromJson(dynamic json) {
+    name = json['name'];
+    if (json['transitions'] != null) {
+      transitions = [];
+      json['transitions'].forEach((v) {
+        transitions?.add(Transitions.fromJson(v));
+      });
+    }
+    location = json['location'] != null ? Location.fromJson(json['location']) : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    var map = <String, dynamic>{};
+    map['name'] = name;
+    if (transitions != null) {
+      map['transitions'] = transitions?.map((v) => v.toJson()).toList();
+    }
+    if (location != null) {
+      map['location'] = location?.toJson();
+    }
+    return map;
+  }
+
+}
+
+class Location {
+  String? countryCode;
+  int? latitude;
+  int? longitude;
+  String? comments;
+
+  Location({
+      this.countryCode, 
+      this.latitude, 
+      this.longitude, 
+      this.comments});
+
+  Location.fromJson(dynamic json) {
+    countryCode = json['country_code'];
+    latitude = json['latitude'];
+    longitude = json['longitude'];
+    comments = json['comments'];
+  }
+
+  Map<String, dynamic> toJson() {
+    var map = <String, dynamic>{};
+    map['country_code'] = countryCode;
+    map['latitude'] = latitude;
+    map['longitude'] = longitude;
+    map['comments'] = comments;
+    return map;
+  }
+
+}
+
+class Transitions {
+  int? ts;
+  String? time;
+  int? offset;
+  bool? isdst;
+  String? abbr;
+
+  Transitions({
+      this.ts, 
+      this.time, 
+      this.offset, 
+      this.isdst, 
+      this.abbr});
+
+  Transitions.fromJson(dynamic json) {
+    ts = json['ts'];
+    time = json['time'];
+    offset = json['offset'];
+    isdst = json['isdst'];
+    abbr = json['abbr'];
+  }
+
+  Map<String, dynamic> toJson() {
+    var map = <String, dynamic>{};
+    map['ts'] = ts;
+    map['time'] = time;
+    map['offset'] = offset;
+    map['isdst'] = isdst;
+    map['abbr'] = abbr;
     return map;
   }
 
